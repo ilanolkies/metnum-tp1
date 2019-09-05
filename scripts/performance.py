bc5c0f74da18d2d226a88b2ab3624d3c5bbab0f8
Ilan Olkies <ilanolkies@outlook.com>
2019-09-05 02:33:20 -0300

    Update performance.py

--- scripts/performance.py
+++ scripts/performance.py
@@ -5,34 +5,47 @@ compile = 'g++ src/tp1.cpp -o bin/tp1'
 os.system(compile)
 
 # nombre de archivo .in en carpeta ./input
-INPUT = sys.argv[1]
+archivos = [
+  'test-prob-1',
+  'test-prob-2',
+  'test1',
+  'test2',
+  'test_completo_10_1',
+  'test_completo_100_4',
+  'test_completo_100_8',
+  'test_completo_1000_2',
+  'test_completo_1000_8'
+]
 
-# borra la corrida anterior
-rm = 'rm -r performance/{0} 2> /dev/null'.format(INPUT)
-os.system(rm)
-mkdir = 'mkdir performance/{0}'.format(INPUT)
-os.system(mkdir)
+result = ''
 
-import time
+for INPUT in archivos:
+    # borra la corrida anterior
+    rm = 'rm -r performance/{0} 2> /dev/null'.format(INPUT)
+    os.system(rm)
+    mkdir = 'mkdir performance/{0}'.format(INPUT)
+    os.system(mkdir)
 
-start = time.time()
-run = './bin/tp1 0 input/{}.in output/{}.out'.format(INPUT, INPUT)
-os.system(run)
-end = time.time()
-elpasedEG = end - start
+    import time
 
-start = time.time()
-run = './bin/tp1 4 input/{}.in output/{}.out'.format(INPUT, INPUT)
-os.system(run)
-end = time.time()
-elpasedChol = end - start
+    start = time.time()
+    run = './bin/tp1 0 input/{}.in output/{}.out'.format(INPUT, INPUT)
+    os.system(run)
+    end = time.time()
+    elpasedEG = end - start
+
+    start = time.time()
+    run = './bin/tp1 4 input/{}.in output/{}.out'.format(INPUT, INPUT)
+    os.system(run)
+    end = time.time()
+    elpasedChol = end - start
 
 
-result = '''
+    result += '''
 \\begin{{center}}
     \\begin{{tabular}}{{|c c|}}
         \\hline
-        \multicolumn{{2}}{{|c|}}{{\\textit{{{0}}}}} \\\\
+        Timepo de ejecucion (s) & \\textit{{{0}}} \\\\
         \\hline
         \\hline
         Con Eliminacion Gaussiana & Con Fact. de Cholesky \\\\
@@ -43,6 +56,6 @@ result = '''
 \\end{{center}}
 '''.format(INPUT, elpasedEG, elpasedChol)
 
-f = open('performance/{0}/valores.out'.format(INPUT), 'w')
+f = open('performance/valores.out'.format(INPUT), 'w')
 f.write(result)
 f.close()
