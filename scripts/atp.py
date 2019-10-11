de47d8282b04b80740bd3f5ff2600042578aa6d0
andres nahuel antola <aantola@dc.uba.ar>
2019-10-11 16:18:32 -0300

    atp

--- scripts/atp.py
+++ scripts/atp.py
@@ -23,32 +23,41 @@ f.close()
 # que nuestra matriz no sea innecesariamente grande
 # contamos con la cantidad de equipos que juegan al menos
 # una vez
+cantPartidos = input[1]
 
-
-
-enfrentamientos = input[1:]
+enfrentamientos = input[2:]
 
 seEnfrento = {}
 
+jugadores = {}
+
 for e in enfrentamientos:
   row = e.split(' ')
+
   seEnfrento[row[1]] = True
   seEnfrento[row[3]] = True
+
 
 
 ##Asignamos numero de menor a mayor
 j = 1
 for i in sorted(seEnfrento.keys()):
 	seEnfrento[i] = " {} ".format(str(j))
+
 	j = j+1
 
+
 ##Reemplazamos los valores usando el diccionario
-for i, e in enumerate(input[1:]):
-	i = i+1
+for i, e in enumerate(enfrentamientos):
+	i = i+2
+	#print(e)
 	row = e.split(' ')
 	row[1] = seEnfrento[row[1]]
 	row[3] = seEnfrento[row[3]]
-	input[i] = row
+	jugadores[row[1]] =' {} {} '.format(row[5], row[6])
+	jugadores[row[3]]=' {} {} '.format(row[7], row[8])
+	
+	input[i] = row[:5]
 
 
 
@@ -65,24 +74,185 @@ def listToString(s):
     # return string   
     return str1  
 
-cantPartidos = input[0].split(' ')[1]
-input[0] = str(len(seEnfrento)) + ' ' +str(cantPartidos)
+input[0] = str(len(seEnfrento)) + ' ' + str(cantPartidos) + '\n'
+
+
+
 
 # escribir los resultados en un archivo valido como input
 f = open('input/input_recortado.in', 'w')
-f.writelines(listToString(line)+ '\n' for line in input)
+f.writelines(input[0])
+f.writelines(listToString(line)+ '\n' for line in input[2:])
 f.close()
 
 
-# correr atp
-#os.system('./bin/tp1 0 input/input_recortado.in input/input_recortado.out')
+
+
+##Leer ranking original
+f = open('dataset/atp_results')
+lectura_ranking = f.read().splitlines()
+f.close()
+
+atpP = []
+atp = {}
+##Lectura y conversion
+j = 1
+for e in lectura_ranking:
+	row = e.split(',')
+	
+	if(row[2] in seEnfrento):
+		jugador = seEnfrento[row[2]]
+		atp[int(jugador)] = j;
+		atpP.append(jugador)
+		j+= 1
+	#else:
+	#	atp[int(row[2])] = int(row[1]) 
+	#	atpP.append(' {} '.format(row[2]))
+
+while len(atpP) < 431:
+	atpP.append(' 1 ')
+
+#######Comparacion modificado ########
+
+
+INPUT = 'input_recortado'
+
+# borra la comparcio anterior
+rm = 'rm -r comparacion/{0} 2> /dev/null'.format(INPUT)
+os.system(rm)
+mkdir = 'mkdir comparacion/{0}'.format(INPUT)
+os.system(mkdir)
+
+# corre los 4 metodos y el output en comparacion/INPUT/metodo-x.out
+for i in range(4):
+  run = "./bin/tp1 {0} input/{1}.in comparacion/{1}/metodo-{0}.out".format(i, INPUT)
+
+  os.system(run)
+
+out_dir = 'comparacion/{}'.format(INPUT)
+# retorna el orden por equipo de un ranking dado
+def rankingOrdenado(method):
+  f = open('{}/metodo-{}.out'.format(out_dir, method), 'r')
+  ranking = f.read().splitlines()
+  f.close()
+  rankingConEquipo = [(i+1, float(ranking[i])) for i in range(len(ranking))]
+  rankingConEquipo.sort(key = lambda val : val[1])
+  return [ranking[0] for ranking in rankingConEquipo]
+
+# ordenar los 4 rankings
+cmm = rankingOrdenado(0)
+wp = rankingOrdenado(1)
+ahp = rankingOrdenado(2)
+odi = rankingOrdenado(3)
+
+def diferenciaConATP(ranking):
+	res = []
+	for i,r in enumerate(ranking):
+		if(r in atp.keys()):
+			res.append((r, str(i-atp[int(r)])))
+		else:
+			res.append((r, '-'))
+
+	return res
+#  return [(r, i-atp[int(r)]) for i,r in enumerate(ranking)]
+
+def getArrow(n):
+  if n > 0:
+    return '\\textcolor{green}{$\\uparrow$}'
+  if n < 0: 
+    return '\\textcolor{red}{$\\downarrow$}'
+  return ''
+
+def getDiff(n):
+  if n == 0 or n == '-':
+    return '-'
+  return str(abs(int(n)))
 
 
 
-# imprimir tabla de comparacion de puestos
+
+cmmComparado = diferenciaConATP(cmm)
+wpComparado = diferenciaConATP(wp)
+ahpComparado = diferenciaConATP(ahp)
+odiComparado = diferenciaConATP(odi)
 
 
-# estaria bueno usar los nombres del csv en la tabla
+
+# formato de tabla para latex comparando rankings
+out = '''\\begin{center}
+  \\begin{tabular}{| c || c | c || c | c || c | c || c | c |}
+    \\hline
+    ATP & \multicolumn{2}{c ||}{CMM} & \multicolumn{2}{c ||}{WP} & \multicolumn{2}{c ||}{AHP} & \multicolumn{2}{c |}{ODI} \\\\
+    \hline\hline'''
 
 
-os.system('python scripts/comparacion.py input_recortado')
+for i in range(len(cmm)):
+  out += '''
+    {} & {} & {} {} & {} & {} {} & {} & {} {} & {} & {} {}\\\\'''.format(
+    atpP[i],
+    cmmComparado[i][0],
+    getArrow(cmmComparado[i][1]),
+    getDiff(cmmComparado[i][1]),
+    wpComparado[i][0],
+    getArrow(wpComparado[i][1]),
+    getDiff(wpComparado[i][1]),
+    ahpComparado[i][0],
+    getArrow(ahpComparado[i][1]),
+    getDiff(ahpComparado[i][1]),
+    odiComparado[i][0],
+    getArrow(odiComparado[i][1]),
+    getDiff(odiComparado[i][1]),
+  )
+
+out += '''
+  \\hline
+  \end{tabular}
+\end{center}'''
+
+
+
+
+f = open('{}/resultado_latex.out'.format(out_dir), 'w')
+f.write(out)
+f.close()
+
+
+
+
+###Asignacion de nombres de jugador
+
+f = open('comparacion/input_recortado/resultado_latex.out')
+input = f.read().splitlines()
+f.close()
+
+
+
+for i,e in enumerate(input[5:435]):
+	row = e.split(' ')
+
+	#print row
+	if((' {} '.format(int(row[5]))) in jugadores.keys()):	
+		row[5] = jugadores[' {} '.format(int(row[5]))]
+
+	#print row[8]
+	if((' {} '.format(int(row[8]))) in jugadores.keys()):	
+		row[8] = jugadores[' {} '.format(int(row[8]))]
+
+	#print row[13]
+	if((' {} '.format(int(row[13]))) in jugadores.keys()):	
+		row[13] = jugadores[' {} '.format(int(row[13]))]
+
+	#print row[18]
+	if((' {} '.format(int(row[18]))) in jugadores.keys()):	
+		row[18] = jugadores[' {} '.format(int(row[18]))]
+
+	#print row[23]
+	if((' {} '.format(int(row[23]))) in jugadores.keys()):	
+		row[23] = jugadores[' {} '.format(int(row[23]))]
+
+	input[i+5] = row
+
+f = open('comparacion/input_recortado/resultado_latex.out', 'w')
+f.writelines(listToString(line) + '\n' for line in input[0:20])
+f.writelines(listToString(line) + '\n' for line in input[435:])
+f.close()
--- scripts/convert_atp_dataset.py
+++ scripts/convert_atp_dataset.py
@@ -1,10 +1,10 @@
 import csv
 
-f = open('./in/atp_matches_2015.in', 'w')
+f = open('input/atp_matches_2015.in', 'w')
 
 equipos = {}
 
-with open('./dataset/atp_players.csv') as csv_file:
+with open('dataset/atp_players.csv') as csv_file:
   csv_reader = csv.reader(csv_file, delimiter=',')
   line_count = -1
   for row in csv_reader:
@@ -19,11 +19,12 @@ with open('./dataset/atp_matches_2015.csv') as csv_file:
   line_count = 0
   for row in csv_reader:
     if line_count > 0:
-      partidos.append([row[5], equipos[row[7]], 1, equipos[row[17]], 0])
+      partidos.append([row[5], row[7], 1, row[17], 0, row[10], row[20]])
     line_count += 1
 
 f.write(str(len(equipos)) + '\n')
 f.write(str(len(partidos)) + '\n')
 for partido in partidos:
-  f.write('{} {} {} {} {}\n'.format(partido[0], partido[1], partido[2], partido[3], partido[4]))
+  print(partido)
+  f.write('{} {} {} {} {} {} {}\n'.format(partido[0], partido[1], partido[2], partido[3], partido[4], partido[5], partido[6]))
 f.close()
